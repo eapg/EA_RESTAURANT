69a203210783b782304b31927f7e2d6047461baa
eapg <epenag@gmail.com>
2022-03-17 16:54:02 -0400

    Adding Product Entity

---
+++ src/main/python/entities/product.py
@@ -0,0 +1,11 @@
+# class for the products of the restaurant ;like pizzas, pasta,lasagna
+
+class Product:
+
+    def __init__(self):
+
+        self.id = int
+        self.name = str
+        self.description = str
+        self.ingredients = list
+
