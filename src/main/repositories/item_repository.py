29b360b9f5174e935541732d282c280296715b86
eapg <epenag@gmail.com>
2022-03-23 11:25:01 -0400

    Adding item repository

---
+++ src/main/repositories/item_repository.py
@@ -0,0 +1,30 @@
+# This file has the item repository
+
+from src.main.repositories.generic_repository import GeneralRepository
+
+
+class ItemRepository(GeneralRepository):
+
+    def __init__(self):
+        self.__items = {}
+        self.__current_id = 0
+
+    def add(self, item):
+        item.id = self.__current_id
+        self.__items[item.id] = item
+        self.__current_id += 1
+
+    def get_by_id(self, item_id):
+        return self.__items[item_id]
+
+    def get_all(self):
+        return self.__items.values()
+
+    def delete_by_id(self, item_id):
+        self.__items.pop(item_id)
+
+    def update_by_id(self, item_id, item):
+        current_item = self.get_by_id(item_id)
+        current_item.name = item.name
+
+
