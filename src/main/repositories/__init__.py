d994503b795b3fbca80fcf25df8dc8e53b00a955
eapg <epenag@gmail.com>
2022-03-24 15:55:41 -0400

    Adding order detail repository

--- src/main/repositories/__init__.py
+++ src/main/repositories/__init__.py
@@ -1,5 +1,7 @@
 __all__ = ['generic_repository', 'product_repository', 'item_repository', 'inventory_item_repository',
-           'inventory_repository', 'order_repository']
+           'inventory_repository', 'order_repository', 'order_detail_repository']
+
+
 
 
 
