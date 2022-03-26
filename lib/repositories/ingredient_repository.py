55bf33ea9a107be6fef34067c5f5b06593429444
Reynold Morel <reynoldmorel@gmail.com>
2022-03-26 12:09:05 -0400

    Removed extra underscore for private props

--- lib/repositories/ingredient_repository.py
+++ lib/repositories/ingredient_repository.py
@@ -6,22 +6,22 @@ from lib.repositories.generic_repository import GenericRepository
 class IngredientRepository(GenericRepository):
 
     def __init__(self):
-        self.__ingredients = {}
-        self.__current_id = 0
+        self._ingredients = {}
+        self._current_id = 0
 
     def add(self, ingredient):
-        ingredient.id = self.__current_id
-        self.__ingredients[ingredient.id] = ingredient
-        self.__current_id += 1
+        ingredient.id = self._current_id
+        self._ingredients[ingredient.id] = ingredient
+        self._current_id += 1
 
     def get_by_id(self, ingredient_id):
-        return self.__ingredients[ingredient_id]
+        return self._ingredients[ingredient_id]
 
     def get_all(self):
-        return self.__ingredients.values()
+        return self._ingredients.values()
 
     def delete_by_id(self, ingredient_id):
-        self.__ingredients.pop(ingredient_id)
+        self._ingredients.pop(ingredient_id)
 
     def update_by_id(self, ingredient_id, ingredient):
         current_ingredient = self.get_by_id(ingredient_id)
