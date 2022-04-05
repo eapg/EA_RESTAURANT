1e38d0bc648e42d322ce56724d0bcb9af8c1e704
eapg <epenag@gmail.com>
2022-04-05 10:11:01 -0400

    Adding order detail controller and unit test

---
+++ src/api/controllers/order_detail_controller.py
@@ -0,0 +1,20 @@
+class OrderDetailController:
+    def __init__(self, order_detail_repository):
+        self._order_detail_repository = (
+            order_detail_repository  # order_detailRepository
+        )
+
+    def add(self, order_detail):
+        self._order_detail_repository.add(order_detail)
+
+    def get_by_id(self, order_detail_id):
+        return self._order_detail_repository.get_by_id(order_detail_id)
+
+    def get_all(self):
+        return self._order_detail_repository.get_all()
+
+    def delete_by_id(self, order_detail_id):
+        self._order_detail_repository.delete_by_id(order_detail_id)
+
+    def update_by_id(self, order_detail_id, order_detail):
+        self._order_detail_repository.update_by_id(order_detail_id, order_detail)
