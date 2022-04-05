bdc8606b08026fc16b76a72c0dfe0fec85d10b97
eapg <elidopg@gmail.com>
2022-04-04 20:53:14 -0400

    Adding inventory controller and unit test

---
+++ src/tests/api/controllers/inventory_controller_test.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest import mock
+
+from src.api.controllers.inventory_controller import InventoryController
+from src.tests.utils.fixtures.inventory_fixture import build_inventory, build_inventories
+
+
+class InventoryRepositoryControllerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.inventory_repository = mock.Mock()
+        self.inventory_controller = InventoryController(self.inventory_repository)
+
+    def test_add_inventory_successfully(self):
+        inventory = build_inventory()
+
+        self.inventory_controller.add(inventory)
+
+        self.inventory_repository.add.assert_called_with(inventory)
+
+    def test_get_inventory_successfully(self):
+        inventory = build_inventory()
+
+        self.inventory_repository.get_by_id.return_value = inventory
+
+        expected_inventory = self.inventory_controller.get_by_id(inventory.id)
+
+        self.inventory_repository.get_by_id.assert_called_with(inventory.id)
+        self.assertEqual(expected_inventory.id, inventory.id)
+
+    def test_get_all_inventories_successfully(self):
+        inventories = build_inventories(count=3)
+
+        self.inventory_repository.get_all.return_value = inventories
+
+        expected_inventories = self.inventory_controller.get_all()
+
+        self.inventory_repository.get_all.assert_called()
+        self.assertEqual(expected_inventories, inventories)
+        self.assertEqual(len(expected_inventories), 3)
+
+    def test_delete_an_inventory_successfully(self):
+        self.inventory_controller.delete_by_id(2)
+
+        self.inventory_repository.delete_by_id.assert_called_with(2)
+
+    def test_update_an_inventory_successfully(self):
+        inventory = build_inventory()
+
+        self.inventory_controller.update_by_id(1, inventory)
+
+        self.inventory_repository.update_by_id.assert_called_with(1, inventory)
