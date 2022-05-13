6388c521de3c8ac99d58ffdfa800c29c2c2e8449
eapg <elidopg@gmail.com>
2022-05-12 19:26:32 -0500

    Adding product ingredients by product method in the repository and its test

--- src/lib/repositories/product_ingredient_repository.py
+++ src/lib/repositories/product_ingredient_repository.py
@@ -1,8 +1,11 @@
 # Interface for the repositories through Abstract method
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 from src.lib.repositories.generic_repository import GenericRepository
 
 
 class ProductIngredientRepository(GenericRepository, metaclass=ABCMeta):
-    pass
+
+    @abstractmethod
+    def get_product_ingredients_by_product(self, product):
+        pass
