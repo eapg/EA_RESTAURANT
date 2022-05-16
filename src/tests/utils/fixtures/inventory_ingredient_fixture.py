cc4e4a72cfb328ece596d1a21615479a78779121
unknown <elidopg@gmail.com>
2022-05-16 11:02:53 -0400

    Adding inventory to inventory ingredient entity

--- src/tests/utils/fixtures/inventory_ingredient_fixture.py
+++ src/tests/utils/fixtures/inventory_ingredient_fixture.py
@@ -1,12 +1,14 @@
 from src.lib.entities.inventory_ingredient import InventoryIngredient
 from src.tests.utils.fixtures.ingredient_fixture import build_ingredient
+from src.tests.utils.fixtures.inventory_fixture import build_inventory
 
 
 def build_inventory_ingredient(
-    inventory_ingredient_id=None, ingredient=None, ingredient_quantity=None
+    inventory_ingredient_id=None, ingredient=None, inventory=None, ingredient_quantity=None
 ):
     inventory_ingredient = InventoryIngredient()
     inventory_ingredient.id = inventory_ingredient_id
+    inventory_ingredient.inventory = inventory or build_inventory()
     inventory_ingredient.ingredient = ingredient or build_ingredient()
     inventory_ingredient.ingredient_quantity = ingredient_quantity or 1
 
