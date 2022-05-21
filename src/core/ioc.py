58edeeacf12dececd5b25bc3186f2f33eb5155d3
eapg <elidopg@gmail.com>
2022-05-21 17:07:53 -0400

    refactoring imports to be more consistent

--- src/core/ioc.py
+++ src/core/ioc.py
@@ -1,8 +1,46 @@
 # structure for the ioc to have just one instance
 
-from src.api.controllers import *
-from src.lib.repositories.impl import *
-from src.core import order_manager
+from src.core.order_manager import OrderManager
+from src.lib.repositories.impl.product_ingredient_repository_impl import (
+    ProductIngredientRepositoryImpl,
+)
+from src.lib.repositories.impl.product_repository_impl import ProductRepositoryImpl
+from src.lib.repositories.impl.ingredient_repository_impl import (
+    IngredientRepositoryImpl,
+)
+from src.lib.repositories.impl.inventory_ingredient_repository_impl import (
+    InventoryIngredientRepositoryImpl,
+)
+from src.lib.repositories.impl.inventory_repository_impl import InventoryRepositoryImpl
+from src.lib.repositories.impl.order_repository_impl import OrderRepositoryImpl
+from src.lib.repositories.impl.order_detail_product_repository_impl import (
+    OrderDetailProductRepositoryImpl,
+)
+from src.lib.repositories.impl.order_detail_repository_impl import (
+    OrderDetailRepositoryImpl,
+)
+from src.lib.repositories.impl.chef_repository_impl import ChefRepositoryImpl
+from src.lib.repositories.impl.order_status_history_repository_impl import (
+    OrderStatusHistoryRepositoryImpl,
+)
+from src.api.controllers.product_ingredient_controller import (
+    ProductIngredientController,
+)
+from src.api.controllers.product_controller import ProductController
+from src.api.controllers.ingredient_controller import IngredientController
+from src.api.controllers.inventory_ingredient_controller import (
+    InventoryIngredientController,
+)
+from src.api.controllers.inventory_controller import InventoryController
+from src.api.controllers.order_controller import OrderController
+from src.api.controllers.order_detail_product_controller import (
+    OrderDetailProductController,
+)
+from src.api.controllers.order_detail_controller import OrderDetailController
+from src.api.controllers.chef_controller import ChefController
+from src.api.controllers.order_status_history_controller import (
+    OrderStatusHistoryController,
+)
 
 
 class Ioc:
@@ -13,84 +51,66 @@ class Ioc:
         self._init_controllers()
 
     def _init_order_manager(self):
-        self._instance_ioc["order_manager"] = order_manager.OrderManager()
+        self._instance_ioc["order_manager"] = OrderManager()
 
     def _init_repositories(self):
         self._instance_ioc[
             "product_ingredient_repository"
-        ] = product_ingredient_repository_impl.ProductIngredientRepositoryImpl()
-        self._instance_ioc[
-            "product_repository"
-        ] = product_repository_impl.ProductRepositoryImpl()
-        self._instance_ioc[
-            "ingredient_repository"
-        ] = ingredient_repository_impl.IngredientRepositoryImpl()
+        ] = ProductIngredientRepositoryImpl()
+        self._instance_ioc["product_repository"] = ProductRepositoryImpl()
+        self._instance_ioc["ingredient_repository"] = IngredientRepositoryImpl()
         self._instance_ioc[
             "inventory_ingredient_repository"
-        ] = inventory_ingredient_repository_impl.InventoryIngredientRepositoryImpl()
-        self._instance_ioc[
-            "inventory_repository"
-        ] = inventory_repository_impl.InventoryRepositoryImpl()
-        self._instance_ioc[
-            "order_repository"
-        ] = order_repository_impl.OrderRepositoryImpl()
+        ] = InventoryIngredientRepositoryImpl()
+        self._instance_ioc["inventory_repository"] = InventoryRepositoryImpl()
+        self._instance_ioc["order_repository"] = OrderRepositoryImpl()
         self._instance_ioc[
             "order_detail_product_repository"
-        ] = order_detail_product_repository_impl.OrderDetailProductRepositoryImpl()
-        self._instance_ioc[
-            "order_detail_repository"
-        ] = order_detail_repository_impl.OrderDetailRepositoryImpl()
-        self._instance_ioc[
-            "chef_repository"
-        ] = chef_repository_impl.ChefRepositoryImpl()
+        ] = OrderDetailProductRepositoryImpl()
+        self._instance_ioc["order_detail_repository"] = OrderDetailRepositoryImpl()
+        self._instance_ioc["chef_repository"] = ChefRepositoryImpl()
 
         self._instance_ioc[
             "order_status_history_repository"
-        ] = order_status_history_repository_impl.OrderStatusHistoryRepositoryImpl()
+        ] = OrderStatusHistoryRepositoryImpl()
 
     def _init_controllers(self):
         self._instance_ioc[
             "product_ingredient_controller"
-        ] = product_ingredient_controller.ProductIngredientController(
+        ] = ProductIngredientController(
             self._instance_ioc["product_ingredient_repository"]
         )
-        self._instance_ioc["product_controller"] = product_controller.ProductController(
+        self._instance_ioc["product_controller"] = ProductController(
             self._instance_ioc["product_repository"]
         )
-        self._instance_ioc[
-            "ingredient_controller"
-        ] = ingredient_controller.IngredientController(
+        self._instance_ioc["ingredient_controller"] = IngredientController(
             self._instance_ioc["ingredient_repository"]
         )
         self._instance_ioc[
             "inventory_ingredient_controller"
-        ] = inventory_ingredient_controller.InventoryIngredientController(
+        ] = InventoryIngredientController(
             self._instance_ioc["inventory_ingredient_repository"]
         )
-        self._instance_ioc[
-            "inventory_controller"
-        ] = inventory_controller.InventoryController(
+        self._instance_ioc["inventory_controller"] = InventoryController(
             self._instance_ioc["inventory_repository"]
         )
-        self._instance_ioc["order_controller"] = order_controller.OrderController(
+        self._instance_ioc["order_controller"] = OrderController(
             self._instance_ioc["order_repository"]
         )
         self._instance_ioc[
             "order_detail_product_controller"
-        ] = order_detail_product_controller.OrderDetailProductController(
+        ] = OrderDetailProductController(
             self._instance_ioc["order_detail_product_repository"]
         )
-        self._instance_ioc[
-            "order_detail_controller"
-        ] = order_detail_controller.OrderDetailController(
+        self._instance_ioc["order_detail_controller"] = OrderDetailController(
             self._instance_ioc["order_detail_repository"]
         )
-        self._instance_ioc["chef_controller"] = chef_controller.ChefController(
+        self._instance_ioc["chef_controller"] = ChefController(
             self._instance_ioc["chef_repository"]
         )
         self._instance_ioc[
             "order_status_history_controller"
-        ] = order_status_history_controller.OrderStatusHistoryController(
+        ] = OrderStatusHistoryController(
             self._instance_ioc["order_status_history_repository"]
         )
 
