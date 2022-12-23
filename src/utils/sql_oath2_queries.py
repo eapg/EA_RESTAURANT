3b56f804945a4c30b23633fd2dc33c2447bab30a
Elido P <elidopg@gmail.com>
2022-12-23 11:35:15 -0400

    adding client and user validation for refresh token

--- src/utils/sql_oath2_queries.py
+++ src/utils/sql_oath2_queries.py
@@ -3,18 +3,21 @@
 SQL_QUERY_TO_GET_USER_BY_USERNAME = """
 SELECT *
   FROM users
-  where username = :username
+ WHERE username = :username
+   AND entity_status = 'ACTIVE'
 """
 
 SQL_QUERY_TO_GET_CLIENT_BY_CLIENT_ID = """
 SELECT *
   FROM app_clients
   WHERE client_id = :client_id
+  AND entity_status = 'ACTIVE' 
 """
 SQL_QUERY_TO_GET_CLIENT_BY_ID = """
 SELECT *
   FROM app_clients
   WHERE id = :id
+  AND entity_status = 'ACTIVE'
 """
 
 SQL_QUERY_TO_GET_REFRESH_TOKEN_BY_TOKEN = """
@@ -23,12 +26,22 @@ SELECT *
   where token = :token
 """
 
-SQL_QUERY_TO_GET_REFRESH_TOKEN_BY_ACCESS_AND_REFRESH_TOKEN = """
-SELECT art.*
-  FROM app_refresh_tokens art 
- INNER JOIN app_access_tokens aat 
-    ON art.id = aat.refresh_token_id 
- WHERE art.token = :refresh_token AND aat.token = :access_token
+SQL_QUERY_TO_GET_REFRESH_TOKEN_BY_ACCESS_REFRESH_TOKEN_AND_CLIENT = """
+SELECT aprt.*
+  FROM app_refresh_tokens aprt 
+ INNER JOIN app_access_tokens apat 
+    ON aprt.id = apat.refresh_token_id
+ INNER JOIN app_clients apcl
+    ON apcl.id = aprt.app_client_id
+ WHERE aprt.token = :refresh_token 
+   AND apat.token = :access_token
+   AND apcl.client_id = :client_id
+   AND apcl.entity_status = 'ACTIVE'
+   AND ((EXISTS(SELECT username 
+                  FROM app_client_users apcu  
+                 WHERE apcu.username = :username
+                   AND apcu.entity_status = 'ACTIVE')) 
+   OR (char_length(:username) = 0))
 """
 
 SQL_QUERY_TO_GET_SCOPES_BY_CLIENT_ID = """
@@ -40,7 +53,9 @@ SELECT scope
 SQL_QUERY_TO_GET_CLIENT_USER_BY_USERNAME_AND_CLIENT_ID = """
 SELECT *
   FROM app_client_users
-  WHERE username = :username and app_client_id = :app_client_id
+  WHERE username = :username 
+  AND app_client_id = :app_client_id
+  AND entity_status = 'ACTIVE'
 """
 
 # queries to add data to db
